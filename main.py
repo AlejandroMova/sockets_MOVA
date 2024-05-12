9a76b84c21b1fff57e58ab0400f821e8a682f5fb
AlejandroMova <122325572+AlejandroMova@users.noreply.github.com>
2024-05-12 14:20:23 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -29,10 +29,12 @@ while True:
 
                 data_list = data.decode().split(',')
                 print(data_list)
-                if data_list[1] == key: 
-                    if data_list[0].upper() == 'VERSION': 
-                        print('Revisando version')
-                    elif data_list[0].upper() == 'PULL':
-                        print('Haciendo pull')
                 
-                conn.sendall(data)
+                if data_list[1] == key: 
+                
+                    if data_list[0] == 'VERSION': 
+                        response = 'Revisando version'
+                    elif data_list[0] == 'PULL':
+                        response = 'Haciendo pull'
+                
+                conn.sendall(response.encode())
